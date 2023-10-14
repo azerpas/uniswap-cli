34205061b440b4a77363eb0015ad89d7957795de
azerpas <xskylix@hotmail.fr>
2023-10-14 12:50:03 +0200

    feat: add private key support #13

--- src/wallet.rs
+++ src/wallet.rs
@@ -9,8 +9,8 @@ use anyhow::{Context, Result};
 use bip39::{Mnemonic, Language};
 use directories::ProjectDirs;
 use ethers::{
-    prelude::{k256::ecdsa::SigningKey},
-    signers::{coins_bip39::English, MnemonicBuilder, Wallet},
+    prelude::k256::ecdsa::SigningKey,
+    signers::{coins_bip39::English, MnemonicBuilder, Wallet, LocalWallet},
 };
 
 /// Ask the user for a password to encrypt/decrypt the mnemonic
@@ -34,7 +34,7 @@ pub fn decrypt_wallet_data() -> Result<Wallet<SigningKey>> {
     path = path.join("mnemonic.enc");
     let encrypted_data = match std::fs::read(path.clone()) {
         Ok(data) => data,
-        Err(_) => match save_mnemonic(path) {
+        Err(_) => match save_wallet(path) {
             Ok(data) => data,
             Err(e) => return Err(e),
         },
@@ -45,53 +45,71 @@ pub fn decrypt_wallet_data() -> Result<Wallet<SigningKey>> {
         Err(err) => anyhow::bail!("Failed to get password from settings: {}", err),
     };
     let decrypted = decrypt(bs58::decode(&encrypted_data).into_vec().unwrap(), password)?;
-    let ph = std::str::from_utf8(&decrypted).unwrap();
-    let index = 0u32;
-    let wallet = MnemonicBuilder::<English>::default()
-        .phrase(ph)
-        .index(index)?
-        .build()?;
+    let decrypted_as_str: &str = std::str::from_utf8(&decrypted).unwrap();
+
+    let wallet: Wallet<SigningKey>;
+
+    if is_valid_seed_phrase(decrypted_as_str) {
+        wallet = MnemonicBuilder::<English>::default()
+            .phrase(decrypted_as_str)
+            .index(0u32)?
+            .build()?;
+    } else if is_valid_private_key(decrypted_as_str) {
+        wallet = decrypted_as_str.parse::<LocalWallet>()?;
+    } else {
+        anyhow::bail!("Invalid seed phrase or private key");
+    }
+
     Ok(wallet)
 }
 
-/// Ask the user for a mnemonic/seed phrase/recovery phrase/... 
+/// Ask the user for a mnemonic/seed phrase/recovery phrase/private key... 
 /// 
-/// More informations here: https://www.ledger.com/academy/glossary/seed-phrase
+/// More informations here: 
+/// - https://www.ledger.com/academy/glossary/seed-phrase
+/// - https://www.ledger.com/academy/glossary/private-key
 /// 
 /// ## Returns
-/// The mnemonic as a string
-fn ask_for_mnemonic() -> Result<String> {
-    print!("Enter mnemonic/seedphrase: ");
+/// The mnemonic or private key as a string
+fn ask_for_wallet() -> Result<String> {
+    print!("Enter a mnemonic/seedphrase or a private key: ");
     stdout().flush()?;
 
-    let mut mnemonic = String::new();
-    stdin().read_line(&mut mnemonic)?;
+    let mut input = String::new();
+    stdin().read_line(&mut input)?;
 
-    if !is_valid_seed_phrase(&mnemonic) {
-        println!("\nInvalid seed phrase, please try again");
-        return ask_for_mnemonic();
+    // Check if the input is a mnemonic
+    if is_valid_seed_phrase(&input) {
+        Ok(input.trim().to_string())
+    } 
+    // Check if the input is a private key
+    else if is_valid_private_key(&input) {
+        Ok(input.trim().to_string())
+    } 
+    // If the input is not a mnemonic or a private key, ask again
+    else {
+        println!("\nInvalid seed phrase or private key, please try again");
+        return ask_for_wallet();
     }
-
-    Ok(mnemonic.trim().to_string())
 }
 
-/// Encrypt the mnemonic with the password given by the user and save it in a file
+/// Encrypt the wallet with the password given by the user and save it in a file
 /// 
 /// ## Arguments
-/// * `path` - The path to the file where the mnemonic will be saved
+/// * `path` - The path to the file where the wallet will be saved
 /// 
 /// ## Returns
-/// The encrypted mnemonic as a vector of base58 encoded bytes
-fn save_mnemonic(path: PathBuf) -> Result<Vec<u8>> {
-    let mnemonic = ask_for_mnemonic().context("Could not read the mnemonic from stdin")?;
+/// The encrypted wallet as a vector of base58 encoded bytes
+fn save_wallet(path: PathBuf) -> Result<Vec<u8>> {
+    let wallet_as_string = ask_for_wallet().context("Could not read the wallet from stdin")?;
     let password = get_password().context("Could not read the password from stdin")?;
     let nonce = nonce().context("Could not generate a nonce")?;
     let encrypted_data =
-        encrypt(mnemonic, password, nonce).context("Could not encrypt the mnemonic")?;
-    let mut file = File::create(path).context("Could not create the mnemonic file")?;
+        encrypt(wallet_as_string, password, nonce).context("Could not encrypt the wallet")?;
+    let mut file = File::create(path).context("Could not create the wallet file")?;
     let data = bs58::encode(encrypted_data.clone()).into_vec();
     file.write_all(&data)
-        .context("Could not write the mnemonic file")?;
+        .context("Could not write the wallet file")?;
     Ok(data)
 }
 
@@ -115,9 +133,18 @@ fn is_valid_seed_phrase(seed_phrase: &str) -> bool {
     match Mnemonic::from_phrase(seed_phrase, Language::English) {
         Ok(_) => true,
         Err(e) => {
+            #[cfg(debug_assertions)]
             println!("Error: {}", e);
+
             false
         }
+    }
+}
+
+fn is_valid_private_key(private_key: &str) -> bool {
+    match private_key.parse::<LocalWallet>() {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
 
@@ -147,6 +174,17 @@ mod tests {
     fn test_invalid_length() {
         let seed_phrase = "pistol maple duty lunch canyon critic lava penalty"; // Only 8 words
         assert!(!is_valid_seed_phrase(seed_phrase));
+    }
+
+    #[test]
+    fn test_valid_private_key() {
+        let private_key = "dcf2cbdd171a21c480aa7f53d77f31bb102282b3ff099c78e3118b37348c72f7";
+        assert!(is_valid_private_key(private_key));
+    }
+
+    #[test]
+    fn test_invalid_private_key() {
+        assert!(is_valid_private_key("invalid private key") == false);
     }
 
     #[test]
